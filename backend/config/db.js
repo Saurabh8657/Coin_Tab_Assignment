7aa83e9a772ca3cf26872187342a795c49b2616d
Saurabh Ganguly <saurabhganguly38@gmail.com>
2024-04-10 00:57:27 +0530

    adding frontend + backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: 'mysql-14fb0413-cointab-assignment.b.aivencloud.com',
-    user:'avnadmin',
-    password:'AVNS_AaxFhIv2TcTb6LxpfIv',
-    database: 'CoinTabAssignment'
-})
 // const db = mysql.createConnection({
-//     host: 'localhost',
-//     user:'root',
-//     password:'root123',
+//     host: 'mysql-14fb0413-cointab-assignment.b.aivencloud.com',
+//     user:'avnadmin',
+//     password:'AVNS_AaxFhIv2TcTb6LxpfIv',
 //     database: 'CoinTabAssignment'
 // })
+const db = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'root123',
+    database: 'CoinTabAssignment'
+})
 
 db.connect((err)=>{
     if(err) console.log(`error connecting ${err}`);
